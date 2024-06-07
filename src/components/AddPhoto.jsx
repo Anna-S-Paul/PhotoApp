f6d66710646764d162a80d392d013c7b09bad025
AnnaJ <annaspaulimp@gmail.com>
2024-06-08 02:12:32 +0530

    successful

---
+++ src/components/AddPhoto.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import NavPhoto from './NavPhoto'
+
+const AddPhoto = () => {
+  return (
+    <div>
+        <NavPhoto/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Photo ID:</label>
+<input type="number" name="" id="" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label"> Name:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Storage space:</label>
+<input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Photo:</label>
+<input type="file" name="" id="" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<button className="btn btn-success">Upload</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddPhoto
