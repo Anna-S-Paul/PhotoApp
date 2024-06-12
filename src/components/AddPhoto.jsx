ec5f31c688e85c7362bfc903b42d05f4ba490bd3
AnnaJ <annaspaulimp@gmail.com>
2024-06-12 09:13:10 +0530

    updated

--- src/components/AddPhoto.jsx
+++ src/components/AddPhoto.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavPhoto from './NavPhoto'
+import axios from 'axios'
 
 const AddPhoto = () => {
+    const [data,setData]=useState(
+{
+"photoid":"",
+"name":"",
+"stospa":"",
+"photo":""
+
+}
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8086/add",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch().finally()
+    }
   return (
     <div>
         <NavPhoto/>
@@ -12,30 +38,30 @@ const AddPhoto = () => {
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 <label htmlFor="" className="form-label">Photo ID:</label>
-<input type="number" name="" id="" className="form-control" />
+<input type="text" name="photoid" id="" className="form-control" value={data.photoid} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 <label htmlFor="" className="form-label"> Name:</label>
-<input type="text" className="form-control" />
+<input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 <label htmlFor="" className="form-label">Storage space:</label>
-<input type="text" className="form-control" />
+<input type="text" className="form-control" name='stospa' value={data.stospa} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 <label htmlFor="" className="form-label">Photo:</label>
-<input type="file" name="" id="" className="form-control" />
+<input type="text" name="photo" id="" className="form-control" value={data.photo} onChange={inputHandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-<button className="btn btn-success">Upload</button>
+<button className="btn btn-success" onClick={readValue}>Upload</button>
 
                         </div>
                     </div>
